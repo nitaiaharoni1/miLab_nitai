e409e23249d543f416deda94b017fcf6c0a4afa8
nitaiaharoni1 <nitaiaharoni1@gmail.com>
2017-12-26 14:12:44 +0200

    v1.2

--- NodeJS/ex02/ex02.js
+++ NodeJS/ex02/ex02.js
@@ -5,15 +5,9 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/getFile/:filename', (req, res) => {
-	let readStream = fs.createReadStream(`${req.query.filename}.txt` , (err, data) => {
-        if (err) {
-            res.send("file not found");
-            return;
-        };
+app.get('/files/:filename', (req, res) => {
+	fs.createReadStream(`${req.params.filename}.txt`).pipe(res);
 	});
-	readStream.pipe(res);
-});
 
 app.listen(app.get('port'), () =>
     console.log('App listening on port: ' + app.get('port')));
